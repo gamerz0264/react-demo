1df325e1ab1d99dc6c1f257c19f8fb9f459a15be
= <=>
2023-07-09 19:46:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from '@mui/material';
+
+function App() {
+
+  const name = 'John Doe';
+
+  return (
+    <>
+      <h1>React Rough Project Demo</h1>
+      <Button variant='contained'>Sample Button</Button>
+      <h2>Hello World</h2>
+    </>
+  )
+}
+
+export default App
